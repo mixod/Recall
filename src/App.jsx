ff7d3436b5ea225c21f4bb9a7214232b8d6a4743
Sangram Thapa <thapasangram64@gmail.com>
2025-02-28 19:12:29 +0545

    starting routing

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
+import Contact from "./Pages/Contact/Contact";
+import Nav from "./Content/Navigation/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
