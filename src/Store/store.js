cc120a69234adef51f996e01d344d0b57aa6d277
Sangram Thapa <thapasangram64@gmail.com>
2025-03-02 11:58:49 +0545

    added

--- src/Store/store.js
+++ src/Store/store.js
@@ -3,9 +3,8 @@ import { create } from "zustand";
 const useStore = create((set) => (
     {
         number: 0,
-        increase: () => set((state) => ({
-            number: state.number + 1
-        }))
+        increase: () => set((state) => ({ number: state.number + 1 }
+        ))
     }
 ))
 export default useStore;
