56ddf1ef1780687d3abeec905fc585a5633bfce9
Sangram Thapa <thapasangram64@gmail.com>
2025-03-02 12:17:26 +0545

    added

--- src/Content/Footer/Footer.jsx
+++ src/Content/Footer/Footer.jsx
@@ -1,10 +1,17 @@
 import { useContext } from "react";
 import { NewContext } from "../../Pages/DataContent";
+import { useStore } from "zustand";
 
 function Footer() {
   const { theme } = useContext(NewContext);
+  const { number, increase } = useStore();
   return (
     <div style={{ color: theme.color, backgroundColor: theme.background }}>
+      <div>
+        Current NUmber:{number}
+        <br />
+        <button onClick={increase}>Click</button>
+      </div>
       Footer
     </div>
   );
