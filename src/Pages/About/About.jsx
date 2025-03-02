6fa1a91e69c881aa40b468655b07d4e09bac0455
Sangram Thapa <thapasangram64@gmail.com>
2025-03-02 11:44:14 +0545

    correct

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,5 +1,5 @@
 function About() {
-  return <div>About</div>;
+  return <div className="text-2xl">About</div>;
 }
 
 export default About;
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 function Contact() {
-  return <div>Contact</div>;
+  return <div className="text-3xl">Contact</div>;
 }
 
 export default Contact;
