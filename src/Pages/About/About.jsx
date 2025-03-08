8b2acfc800df0caca37c18b8e81b87ff91280e52
Sangram Thapa <thapasangram64@gmail.com>
2025-03-08 19:31:34 +0545

    added

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,12 +4,20 @@ import { NewContext } from "../DataContent";
 function About() {
   const { theme } = useContext(NewContext);
   return (
-    <div
-      className="text-2xl"
-      style={{ color: theme.color, backgroundColor: theme.background }}
-    >
-      About
-    </div>
+    <>
+      <div
+        className="text-2xl"
+        style={{ color: theme.color, backgroundColor: theme.background }}
+      >
+        About
+      </div>
+      <div
+        className="text-2xl"
+        style={{ color: theme.color, backgroundColor: theme.background }}
+      >
+        About
+      </div>
+    </>
   );
 }
 
