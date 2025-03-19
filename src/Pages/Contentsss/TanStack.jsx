9d460ecfafaa764bb294a52e0ef2ef107967e672
Sangram Thapa <thapasangram64@gmail.com>
2025-03-19 13:11:59 +0545

    post method

--- src/Pages/Contentsss/TanStack.jsx
+++ src/Pages/Contentsss/TanStack.jsx
@@ -1,4 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Button } from "antd";
 import axios from "axios";
 import React from "react";
 import { NavLink } from "react-router-dom";
@@ -7,10 +8,41 @@ const getFunction = async () => {
   const { data } = await axios.get("https://fakestoreapi.com/products");
   return data;
 };
+const postFunction = async (productData) => {
+  const { data } = await axios.post(
+    "https://fakestoreapi.com/products",
+    productData
+  );
+  return data;
+};
+
 function TanStack() {
   const { data } = useQuery({ queryKey: ["id"], queryFn: getFunction });
+  const mutation = useMutation({
+    mutationFn: postFunction,
+    onSuccess: (res) => {
+      console.log("respo", res);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const handleSubmit = () => {
+    const productData = {
+      title: "dnckdscs",
+      price: "15",
+      description: "jkafvf vaflvjfv oacfvlsv aj ",
+      image: "fvdblfdhvb ",
+      category: "sdahvjs",
+    };
+    mutation.mutate(productData);
+  };
   return (
     <div className="grid grid-cols-3 justify-center items-center max-w-6xl mx-auto">
+      <div>
+        <Button onClick={handleSubmit}>CLickkk</Button>
+      </div>
       {data?.map((item) => (
         <div key={item.id}>
           <div>
