f0cd9a2726728210f0422a34b766943d7491cd35
Sangram Thapa <thapasangram64@gmail.com>
2025-03-19 11:29:05 +0545

    added tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+import ReactDom from "react-dom/client";
+ReactDom.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
