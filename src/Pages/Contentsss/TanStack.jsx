1c9e1aea233201272c29b1a5127c573f09f515cf
Sangram Thapa <thapasangram64@gmail.com>
2025-03-19 12:32:56 +0545

    get method

--- src/Pages/Contentsss/TanStack.jsx
+++ src/Pages/Contentsss/TanStack.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const getFunction = async () => {
   const { data } = await axios.get("https://fakestoreapi.com/products");
@@ -9,8 +10,21 @@ const getFunction = async () => {
 function TanStack() {
   const { data } = useQuery({ queryKey: ["id"], queryFn: getFunction });
   return (
-    <div className="flex justify-center items-center max-w-6xl mx-auto">
-      TanStack
+    <div className="grid grid-cols-3 justify-center items-center max-w-6xl mx-auto">
+      {data?.map((item) => (
+        <div key={item.id}>
+          <div>
+            <img
+              src={item.image}
+              alt={item.title}
+              className=" w-48 h-48 flex mb-2"
+            />
+            <NavLink to={"/home/" + item.id}>
+              <h2>{item.title}</h2>
+            </NavLink>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from "react";
+import ReactDom from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import ReactDom from "react-dom/client";
-const queryClient = new QueryClient();
+const queryCLient = new QueryClient();
+
 ReactDom.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
+  <QueryClientProvider client={queryCLient}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
