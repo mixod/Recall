5ce773a2136a6e791b778272a51a465ac57d9697
Sangram Thapa <thapasangram64@gmail.com>
2025-03-19 11:31:44 +0545

    added tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,9 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDom from "react-dom/client";
+const queryClient = QueryClient;
 ReactDom.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
